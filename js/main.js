74442ecccce9a8beee1dd543a26098da62f63bfc
People34 <toha1997890@gmail.com>
2022-07-14 21:58:45 +0300

    add adaptive header

--- js/main.js
+++ js/main.js
@@ -15,3 +15,23 @@ $('.head').click(function () {
     $(this).parent().find('.accordion__img').toggleClass('accordion__img-animate');
     $(this).parent().find('.content').slideToggle(180);
 });
+
+
+const menu = document.querySelector(".hamburger-menu__list");
+const menuItems = document.querySelectorAll(".hamburger-menu__item-link");
+const hamburger= document.querySelector(".hamburger");
+const menuIcon = document.querySelector(".hamburger-img");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
